810d85314dd43e6e790d38243ee181050f15641a
Uday2kranth <udaykranth01@gmail.com>
2025-11-03 18:53:13 +0530

    practice questions  day1 challenge of 21 days sql challege

---
+++ day1/day1.sql
@@ -0,0 +1,11 @@
+SELECT * FROM patients;
+
+SELECt patient_id , name , age FROM patients;
+
+SELECT * FROM  services_weekly LIMIT 10;
+
+SELECT * FROM services_weekly LIMIT 10;
+
+SELECT DISTINCT service
+FROM services_weekly
+ORDER BY service;
