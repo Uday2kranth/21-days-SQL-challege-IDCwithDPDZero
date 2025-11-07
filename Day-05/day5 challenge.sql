f64bf9d94ebcdbf6f68208c4c38c08c829d8d715
Uday2Kranth <udaykranth01@gmail.com>
2025-11-07 18:43:12 +0530

    fixing the code issus in the day 5 challenge code

--- Day-05/day5 challenge.sql
+++ Day-05/day5 challenge.sql
@@ -1,7 +1,7 @@
-
 -- Question: Calculate the total number of patients admitted, total patients refused, 
 -- and the average patient satisfaction across all services and weeks. Round the average satisfaction to 2 decimal places.
-SELECT COUNT(patients_admitted) AS total_patients_in,
-COUNT(patients_refused) AS total_patients_out  , 
-ROUND(AVG(patient_satisfaction),2) AS overall_avg_satisfaction 
-FROM Services_weekly GROUP BY service, week ;
+
+SELECT SUM(patients_admitted) AS total_patients_admitted , SUM(patients_refused ) AS total_patient_refused, 
+ROUND(AVG(patient_satisfaction),2) AS  AVG_patient_satisfaction FROM services_weekly;
+
+
