2d7e48389f17b17c17821c11c47d0e3a067ccaa6
Uday2kranth <udaykranth01@gmail.com>
2025-11-10 21:16:42 +0530

    confirming the code for the day 7 challenge is correct or not

--- Day-07/day7 pratice questions.sql
+++ Day-07/day7 pratice questions.sql
@@ -1,3 +1,15 @@
-SELECT *FROM patients ;
-SELECT satisfaction from patients where name = 'Julia Torres';
-SELECT name,satisfaction  FROM patients WHERE satisfaction >=96 ORDER BY satisfaction ASC limit 10;
+SELECT * FROM staff_schedule;
+SELECT * FROM services_weekly;
+SELECT * FROM patients;
+--  Practice Questions:
+
+-- 1. Find services that have admitted more than 500 patients in total.
+SELECT service , COUNT(*) AS admit_count FROM patients  GROUP BY SERVICE  HAVING Count(*) > 500 ;
+SELECT service , COUNT(*) AS admit_count FROM patients  where service = 'surgery' GROUP BY SERVICE  HAVING Count(*) < 500 ;
+
+
+-- 2. Show services where average patient satisfaction is below 75.
+SELECT service, AVG(satisfaction) as avg_satifaction_score FROM  patients  GROUP BY service  Having AVG(satisfaction) <75;
+
+-- 3. List weeks where total staff presence across all services was less than 50.
+SELECT week , SUM(present) total_weekly_presents_staff  FROM  staff_schedule GROUP BY  week HAVING SUM (present) < 50;
