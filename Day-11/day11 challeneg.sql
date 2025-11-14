64800c726ad4f6ac09f766bf8f3d909f08375129
Uday2kranth <udaykranth01@gmail.com>
2025-11-14 14:33:53 +0530

    checking everything is upto date or not

--- Day-11/day11 challeneg.sql
+++ Day-11/day11 challeneg.sql
@@ -1,3 +1,8 @@
-SELECT * from patients;
-select DISTINCT service FROM patients;
-SELECT
+-- SELECT * from services_weekly;
+-- Find all unique combinations of service and event type from the services_weekly table where events are not null or none,
+--  along with the count of occurrences for each combination. Order by count descending.
+
+
+select DISTINCT service, event ,COUNT(*) as event_count FROM services_weekly where event !='none' and event IS NOT NULL
+GROUP BY service,event
+ORDER BY event_count DESC;
