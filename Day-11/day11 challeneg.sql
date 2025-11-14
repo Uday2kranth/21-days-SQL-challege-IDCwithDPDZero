73452910a7d4d0280ae5e6e182a36010ca8255fb
Uday2kranth <udaykranth01@gmail.com>
2025-11-14 14:47:52 +0530

    adding stylized images of the todays challenge code and captized keywords in the todays challenge file

--- Day-11/day11 challeneg.sql
+++ Day-11/day11 challeneg.sql
@@ -3,6 +3,9 @@
 --  along with the count of occurrences for each combination. Order by count descending.
 
 
-select DISTINCT service, event ,COUNT(*) as event_count FROM services_weekly where event !='none' and event IS NOT NULL
+SELECT DISTINCT service, event ,COUNT(*) AS event_count
+FROM services_weekly 
+WHERE 
+     event !='none' and event IS NOT NULL
 GROUP BY service,event
 ORDER BY event_count DESC;
