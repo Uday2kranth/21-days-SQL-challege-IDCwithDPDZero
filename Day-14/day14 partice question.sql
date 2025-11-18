89ccec9a06c0bf8ab2e115b238345ac04a1485df
Uday2kranth <udaykranth01@gmail.com>
2025-11-18 22:20:35 +0530

    captilized the function keywords and operators in the pratice question code

--- Day-14/day14 partice question.sql
+++ Day-14/day14 partice question.sql
@@ -6,14 +6,22 @@ select * from services_weekly;
 select* from patients;
 -- 1. Show all staff members and their schedule information (including those with no schedule entries).
 
-select s.staff_id,
-s.staff_name,
-s.role,
-s.service, count(ss.week) AS weeks_scheduled ,
-sum(coalesce(ss.present,0)) AS weeks_present
-from staff s left join staff_schedule ss on s.staff_id= ss.staff_id
-group by s.staff_name,s.staff_id,s.role,s.service;
+SELECT 
+	s.staff_id,
+	s.staff_name,
+	s.role,
+	s.service,
+	COUNT(ss.week) AS weeks_scheduled ,
+	SUM(COALESCE(ss.present,0)) AS weeks_present
+FROM staff s LEFT JOIN staff_schedule ss ON s.staff_id= ss.staff_id
+	GROUP BY s.staff_name,s.staff_id,s.role,s.service;
 -- 2. List all services from services_weekly and their corresponding staff (show services even if no staff assigned).
-select distinct sw.service, s.staff_name from services_weekly sw left join staff s on sw.service=s.service;
+SELECT DISTINCT sw.service, s.staff_name FROM  services_weekly sw LEFT JOIN  staff s ON sw.service=s.service;
 -- 3. Display all patients and their service's weekly statistics (if available).
-select distinct p.name,sw.service,sw.patient_satisfaction,sw.patients_admitted from patients p left join services_weekly sw on p.service=sw.service;
+SELECT 	DISTINCT 
+		p.name,
+		sw.service,
+		sw.patient_satisfaction,
+		sw.patients_admitted 
+FROM 
+	patients p 	LEFT JOIN  services_weekly sw ON  p.service=sw.service;
